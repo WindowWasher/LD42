0455372692c7605e8901557db6224b1cd7d2f0da
Dylan Andersen <dander7@hotmail.com>
2018-08-11 11:58:30 -0600

    Added Player health bar

---
+++ LD42/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Image healthBarFg;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<Health>().OnHealthChange += UpdateHealthBar;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void UpdateHealthBar(int currentHealth, int maxHealth)
+    {
+        healthBarFg.fillAmount = (float)currentHealth / maxHealth;
+    }
+}
