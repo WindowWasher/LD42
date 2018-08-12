b3a9134ed3d1358fa7cd3d5796f7a69181fa58ce
Dylan Andersen <dander7@hotmail.com>
2018-08-12 09:40:13 -0600

    Improved arrow collision

--- LD42/Assets/Scripts/Attack/Arrow.cs
+++ LD42/Assets/Scripts/Attack/Arrow.cs
@@ -13,7 +13,7 @@ public class Arrow : MonoBehaviour {
 
     private Timer deathTimer = null;
 
-    private float force = 300000f;
+    private float force = 3000f;
     public int damage = 10;
 
 	// Use this for initialization
@@ -56,6 +56,8 @@ public class Arrow : MonoBehaviour {
         this.transform.position = startPosition;
 
 
+        //this.transform.LookAt(targetPosition + new Vector3(-0.2f, 0, 0));
+
         this.transform.LookAt(targetPosition);
 
         //this.transform.position = startPosition;
@@ -80,37 +82,48 @@ public class Arrow : MonoBehaviour {
 
         //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         //cube.transform.position = targetPosition;
+        //cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.layer == LayerMask.NameToLayer("PlayerLayer"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("PlayerLayer"))
         {
             return;
         }
+
+        Debug.Log("Hit: " + other.gameObject.name);
 
         Health otherHealth = other.gameObject.GetComponentInParent<Health>();
 
         if (otherHealth != null)
         {
-            Enemy enemy = other.GetComponentInParent<Enemy>();
-            EnemyBodyPart enemyPart = other.GetComponent<EnemyBodyPart>();
+            Enemy enemy = other.gameObject.GetComponentInParent<Enemy>();
+            EnemyBodyPart enemyPart = other.gameObject.GetComponent<EnemyBodyPart>();
             if (enemy == null || enemyPart == null)
                 return;
             //if(enemy != null && enemyPart != null)
             //{
-                enemy.HitWithArrow(enemyPart, this);
+            enemy.HitWithArrow(enemyPart, this);
             //}
             //else {
             //    otherHealth.TakeDamage(damage);
             //}
-            
+
         }
         else
         {
             deathTimer = new Timer();
             deathTimer.Start(60f);
         }
+
+        var heading = other.contacts[0].point - this.transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance; // This is now the normalized direction.
+
+        Debug.Log("Distance: " + distance.ToString());
+
+        this.transform.position = this.transform.position + direction * distance / 2;
 
         // do this to avoid scaling issues
         GameObject newObj = new GameObject();
@@ -126,6 +139,58 @@ public class Arrow : MonoBehaviour {
         this.GetComponent<CapsuleCollider>().enabled = false;
 
 
-
     }
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.gameObject.layer == LayerMask.NameToLayer("PlayerLayer"))
+    //    {
+    //        return;
+    //    }
+
+    //    Health otherHealth = other.gameObject.GetComponentInParent<Health>();
+
+    //    if (otherHealth != null)
+    //    {
+    //        Enemy enemy = other.GetComponentInParent<Enemy>();
+    //        EnemyBodyPart enemyPart = other.GetComponent<EnemyBodyPart>();
+    //        if (enemy == null || enemyPart == null)
+    //            return;
+    //        //if(enemy != null && enemyPart != null)
+    //        //{
+    //            enemy.HitWithArrow(enemyPart, this);
+    //        //}
+    //        //else {
+    //        //    otherHealth.TakeDamage(damage);
+    //        //}
+            
+    //    }
+    //    else
+    //    {
+    //        deathTimer = new Timer();
+    //        deathTimer.Start(60f);
+    //    }
+
+    //    var heading = other.transform.position - this.transform.position;
+    //    var distance = heading.magnitude;
+    //    var direction = heading / distance; // This is now the normalized direction.
+
+    //    //this.transform.position = this.transform.position + direction * distance / 0.5f;
+
+    //    // do this to avoid scaling issues
+    //    GameObject newObj = new GameObject();
+    //    this.transform.parent = newObj.transform;
+    //    newObj.transform.parent = other.transform;
+    //    //newObj.transform.parent = other.GetComponentInParent<Enemy>().GetComponentInChildren<EnemyBodyPart>().gameObject.transform;
+
+    //    this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    //    this.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    //    this.GetComponent<Rigidbody>().useGravity = false;
+    //    this.GetComponent<Rigidbody>().isKinematic = true;
+    //    //this.transform.parent = other.transform;
+    //    this.GetComponent<CapsuleCollider>().enabled = false;
+
+
+
+    //}
 }
