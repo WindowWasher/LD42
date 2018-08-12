e109a05f87eefae57d88618ad8287e8f24e25e62
Dylan Andersen <dander7@hotmail.com>
2018-08-11 20:31:04 -0600

    sort of working bow and arrow

---
+++ LD42/Assets/Scripts/Attack/Arrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Arrow : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(this.transform.forward * Time.deltaTime);
+    }
+
+    public void setTargetPosition(Vector3 targetPosition)
+    {
+        this.transform.LookAt(targetPosition);
+    }
+}
